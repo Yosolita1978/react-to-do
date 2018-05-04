14552c55509b687e12360fe6754ab17c1fe6de6e
cristina <yosola@gmail.com>
2018-05-04 13:57:01 -0700

    New event handler that allows me add todos

--- src/App.js
+++ src/App.js
@@ -3,16 +3,53 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <ul>
-          <ToDo />
-          <ToDo />
-      </ul>
-      </div>
-    );
+   constructor(props) {
+     super(props);
+      this.state = {
+        todos: [
+          { description: 'Buy my new desk', isCompleted: false },
+          { description: 'Throw the dishes away', isCompleted: false },
+          { description: 'Buy new dishes', isCompleted: false }
+        ],
+        newTodoDescription: ''
+      };    
+   }
+
+   handleChange(e) {
+     this.setState({ newTodoDescription: e.target.value })
+   }
+
+
+   handleSubmit(e) {
+     e.preventDefault();
+     if (!this.state.newTodoDescription) { return }
+     const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+     this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+   }
+
+   toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
   }
-}
+ 
+   render() {
+     return (
+       <div className="App">
+         <ul>
+           { this.state.todos.map( (todo, index) => 
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+
+           )}
+         </ul>
+         <form onSubmit={ (e) => this.handleSubmit(e) }>
+           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+           <input type="submit" />
+         </form>
+       </div>
+     );
+   }
+ }
 
 export default App;
